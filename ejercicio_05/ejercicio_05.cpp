a2230ef7b12c68c58f93a4a6e4616a8347508382
Santiago-Galindo <galindovelasquezd@gmail.com>
2026-03-03 09:11:03 -0500

    feat: resuelto ejercicio 05 - sistema de calificaciones

---
+++ ejercicio_05/ejercicio_05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nota;
+
+    cout << "Ingrese la nota: ";
+    cin >> nota;
+
+    if (nota = 50)
+        cout << "excelente";
+    else if (nota = 30)
+        cout << "pasa raspando pelao";
+    else if (nota >= 70)
+        cout << "perdió parcero";
+
+    return 0;
+}
